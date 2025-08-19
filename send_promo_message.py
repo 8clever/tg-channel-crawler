0590ec7ca1e4628df6851d7865044b13506b8b0e
Иван Витяев <godofluck89@gmail.com>
2025-08-19 21:05:41 +0300

    send message promo

---
+++ send_promo_message.py
@@ -0,0 +1,56 @@
+
+import asyncio
+from os import environ
+from dotenv import load_dotenv
+from authorize import authorize
+
+load_dotenv()
+
+file_name = 'blockDAGnetworkOfficial_usernames'
+file_pending = f'{file_name}_pending_invites'
+
+def file_name_to_path (name: str):
+  return f'tmp/{name}.txt'
+
+def save ():
+  with open(file_name_to_path(file_pending), 'w') as f:
+    f.write('\n'.join(users))
+
+users: list[str] = []
+
+try:
+  with open(file_name_to_path(file_pending)) as f:
+    users = f.read().split('\n')
+except:
+  with open(file_name_to_path(file_name)) as f:
+    users = f.read().split('\n')
+  save()
+
+api_id = int(environ.get("TG_API_ID"))
+api_hash = environ.get("TG_API_HASH")
+phone = environ.get("TG_PHONE")
+channel = environ.get("CHANNEL")
+total = len(users)
+
+message = '''🚀 Unlock the Future! 🔬 Join Co.Ca. Lab for exclusive insights, trending tech, and mind-blowing ideas! 💡
+Subscribe now and stay ahead of the curve! 📱 @co_ca_lab'''
+
+async def main ():
+  client = await authorize(phone, api_id, api_hash)
+
+  for id in users:
+    await client.send_message(id, message)
+    print(f'Message sended to: {id}')
+
+    # remove users from pending state
+    users.remove(id)
+
+    # store pending state
+    save()
+    
+    # stop processing, try to one batch per account
+    break
+
+  print("Invite completed")
+
+asyncio.run(main())
